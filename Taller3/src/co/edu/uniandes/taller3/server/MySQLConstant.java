12d8a0e390d38af9d3a1a37fc62fe310b932e8b5
cr.calle <cr.calle@uniandes.edu.co>
2015-05-19 07:48:35 -0500

    User and password db

--- Taller3/src/co/edu/uniandes/taller3/server/MySQLConstant.java
+++ Taller3/src/co/edu/uniandes/taller3/server/MySQLConstant.java
@@ -15,7 +15,7 @@ public class MySQLConstant {
 	public static final String DB_URL = "jdbc:mysql://localhost:3306/ml";
 
 	//  Database credentials
-	public static final String USER = "root";
-	public static final String PASS = "123456";
+	public static final String USER = "usml";
+	public static final String PASS = "12345678";
 	
 }
